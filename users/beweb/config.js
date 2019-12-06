7848896de213c0778142e990f99bf38903e36ffc
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2019-12-06 12:47:00 +0100

    New config handling

--- users/beweb/config.js
+++ users/beweb/config.js
@@ -5,8 +5,57 @@
 class Config {
 
     constructor(config) {
+
         // Store configuration JSON
         this.config = config;
+
+        // Parse composite fields
+        this._parseCompositeFields()
+
+    }
+
+    _parseCompositeFields() {
+
+        let configFields = this.config.fields;
+
+        Object.keys(configFields).forEach((key) => {
+            if(configFields[key].composite) {
+                configFields[key].composite.map(el => {
+
+                    // Capitalize current subfield
+                    el = el.charAt(0).toUpperCase() + el.slice(1);
+                    let newKey = key + el;
+
+                    // Copy parent field
+                    this.config.fields[newKey] = JSON.parse(JSON.stringify(configFields[key]));
+
+                    // Translate subfields as principal fields
+                    if (this.config.fields[newKey].input)
+                        this.config.fields[newKey].input = configFields[key].input + el;
+
+                    if (this.config.fields[newKey].wikidata)
+                        this.config.fields[newKey].wikidata = configFields[key].wikidata + el;
+
+                    if (this.config.fields[newKey].viaf)
+                        this.config.fields[newKey].viaf = configFields[key].viaf + el;
+
+                    if (this.config.fields[newKey].label)
+                        this.config.fields[newKey].label = configFields[key].label + ' ' + el.toUpperCase();
+
+                    delete this.config.fields[newKey].composite;
+
+                });
+
+                // Remove composite field after folding
+                //delete this.config.fields[key];
+
+            }
+        })
+
+    }
+
+    getConfig() {
+        return this.config;
     }
 
     getInputDictionary() {
