88828084b932bf3757c0a802c4be37b776a5ebad
Davide Allavena <DavideAllavena@users.noreply.github.com>
2019-12-09 13:18:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 require('./routes.js')(app);
 
-var server = app.listen(3646, function() {
+var server = app.listen(3645, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Server listening at http://%s:%s', host, port);
