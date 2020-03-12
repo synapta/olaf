c2dca7361ea3daab8062344bd3e119c2a3ba2120
Davide Allavena <davide.allavena@gmail.com>
2020-03-12 18:35:54 +0100

    Added datatable layout

--- app/js/author/beweb-list.js
+++ app/js/author/beweb-list.js
@@ -70,7 +70,41 @@ $(document).ready(() => {
             element.json = JSON.stringify(element);
             output += Mustache.render(template, element);
         });
+
+        
         $( "tbody" ).html(output);
+
+        $('#main-table').DataTable({
+            "columnDefs": [ {
+                "targets": 0,
+                "orderable": false
+            }],
+            "order": [[ 4, "asc" ]],
+            "iDisplayLength": 100,
+            "oLanguage": {
+                "sEmptyTable":     "Nessun dato presente nella tabella",
+                "sInfo":           "Vista da _START_ a _END_ di _TOTAL_ elementi",
+                "sInfoEmpty":      "Vista da 0 a 0 di 0 elementi",
+                "sInfoFiltered":   "(filtrati da _MAX_ elementi totali)",
+                "sInfoPostFix":    "",
+                "sInfoThousands":  ".",
+                "sLengthMenu":     "Visualizza _MENU_ elementi",
+                "sLoadingRecords": "Caricamento...",
+                "sProcessing":     "Elaborazione...",
+                "sSearch":         "Cerca:",
+                "sZeroRecords":    "La ricerca non ha portato alcun risultato.",
+                "oPaginate": {
+                    "sFirst":      "Inizio",
+                    "sPrevious":   "Precedente",
+                    "sNext":       "Successivo",
+                    "sLast":       "Fine"
+                },
+                "oAria": {
+                    "sSortAscending":  ": attiva per ordinare la colonna in ordine crescente",
+                    "sSortDescending": ": attiva per ordinare la colonna in ordine decrescente"
+                }
+            }
+        });
 
         $('.ui.accordion').each(function(i){
             $(this).accordion({ exclusive: false});
