78aa2e0d5f2f99dde0ed512daf7f4c0901306c78
Davide Allavena <davide.allavena@gmail.com>
2020-03-16 18:14:06 +0100

    Cambiata notifica per titolo condiviso

--- users/cobis/parser.js
+++ users/cobis/parser.js
@@ -75,11 +75,11 @@ function getAuthorSimilarOptions(author, options, callback){
                 // Highlight similar titles
                 option.titles.forEach((title, index) => {
                     if(similarTitles[index])
-                        option.titles[index] = '<b>' + title + '</b>';
+                        option.titles[index] = '⟹' + title;
                 });
 
                 // Order title by highlighting
-                option.titles = option.titles.sort((a, b) => b.includes('<b>') - a.includes('<b>'));
+                option.titles = option.titles.sort((a, b) => b.includes('⟹') - a.includes('⟹'));
 
             }
 
