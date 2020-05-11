96804d74be37eb2cb533b6cece6b04384a3f5ddf
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2020-05-11 17:48:45 +0200

    Moved buttons in navbar for Montreux user

--- app/js/author/rendering/montreux.js
+++ app/js/author/rendering/montreux.js
@@ -1,3 +1,26 @@
+// Render navbar
+function renderNavbar() {
+    $.get('/views/template/montreux/navbar.html', (template) => {
+        $('.navbar').html(template).promise().done(showUserToken(params.userToken));
+    })
+}
+
+// Render author card
+function renderAuthorCard(author){
+    $.get('/views/template/montreux/author-card.html', (template) => {
+
+        // Generate output
+        let output = Mustache.render(template, author);
+        // Change page title
+        document.title = author.name + ' - OLAF';
+        // Send output
+        $('#author-card').html(output).promise().done(() => {
+            $('.ui.accordion').accordion({exclusive:false});
+        });
+
+    });
+}
+
 // Render author options
 function renderAuthorOptions(options){
     $.get('/views/template/montreux/author-options.html', (template) => {
