d498f06e6d1fd0a5c31149b690f0fcbc8926c3a3
francescocretti <francesco.cretti@gmail.com>
2020-12-02 18:53:00 +0100

    job page (WIP)

--- app/js/utils/url.js
+++ app/js/utils/url.js
@@ -1,32 +1,30 @@
 // Exports
-function parseUrl(url, paramsMap){
+const getUrlParam = idx => window.location.pathname.split('/')[idx + 1];
 
-    // Get url tokens
-    let tokens = url.split('/');
-    let params = {};
+function parseUrl(url, paramsMap) {
+  // Get url tokens
+  let tokens = url.split('/');
+  let params = {};
 
-    // Get tokens from indexes
-    Object.keys(paramsMap).forEach((key) => {
-        params[key] = tokens[paramsMap[key]];
-    });
+  // Get tokens from indexes
+  Object.keys(paramsMap).forEach((key) => {
+    params[key] = tokens[paramsMap[key]];
+  });
 
-    return params
-
+  return params
 }
 
-function getQueriesFromUrl(url){
+function getQueriesFromUrl(url) {
+  let queries = {};
 
-    let queries = {};
+  // Store queries
+  if (url.includes('?')) {
 
-    // Store queries
-    if (url.includes('?')){
+    let tokens = url.split('?')[1];
+    // Store all queries in a given object
+    tokens.split('&').map((el) => queries[el.split('=')[0]] = el.split('=')[1]);
 
-        let tokens = url.split('?')[1];
-        // Store all queries in a given object
-        tokens.split('&').map((el) => queries[el.split('=')[0]] = el.split('=')[1]);
+  }
 
-    }
-
-    return queries;
-
+  return queries;
 }
