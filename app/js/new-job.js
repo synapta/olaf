6c187824cb03b069c68256f754c1f04e6ab7a109
francescocretti <francesco.cretti@gmail.com>
2020-12-09 14:36:00 +0100

    manage alias already taken

--- app/js/new-job.js
+++ app/js/new-job.js
@@ -5,10 +5,12 @@ const completeFirstStep = async e => {
   const submitButton = e.target.querySelector('button[type="submit"]');
 
   const name = e.target.querySelector('input[name="job-name"]').value;
-  const alias = e.target.querySelector('input[name="job-alias"]').value;
+  const aliasInput = e.target.querySelector('input[name="job-alias"]');
+  const alias = aliasInput.value;
+
 
   if (!aliasRegex.test(alias)) {
-    e.target.querySelector('input[name="job-alias"]').classList.add('wrong');
+    aliasInput.classList.add('wrong');
     e.target.querySelector('.alias-error').classList.add('d-block');
     return;
   }
@@ -21,28 +23,37 @@ const completeFirstStep = async e => {
     item_search: e.target.querySelector('input[name="job-search"]').value || 'Search'
   };
 
-  submitButton.classList.add('disabled', 'loading');
-  const job = await createJob({ name, alias, description, job_type, job_config });
+  toggleLoading(submitButton);
 
-  // TODO - manage error when alias is already taken
+  try {
+    const job = await createJob({ name, alias, description, job_type, job_config });
 
-  // setup second step form
-  setupSecondStep(job.job_id);
-
-  // hide first step
-  const action = await startTransition('.new-job-step.step-1');
-
-  // show second step
-  if (action !== 'hide') {
-    return;
-  }
+    // TODO - manage error when alias is already taken
   
-  await startTransition('.new-job-step.step-2');
-
-  submitButton.classList.remove('disabled', 'loading');
-
-  const newJobStep = document.getElementById('new-job-step');
-  if (newJobStep) newJobStep.innerText = '2';
+    // setup second step form
+    setupSecondStep(job.job_id);
+  
+    // hide first step
+    const action = await startTransition('.new-job-step.step-1');
+  
+    // show second step
+    if (action !== 'hide') {
+      return;
+    }
+    
+    await startTransition('.new-job-step.step-2');
+  
+    toggleLoading(submitButton);
+  
+    const newJobStep = document.getElementById('new-job-step');
+    if (newJobStep) newJobStep.innerText = '2';
+  } catch (error) {
+    if (error.status === 400) {
+      aliasInput.classList.add('wrong');
+      e.target.querySelector('.alias-taken').classList.add('d-block');
+    }
+    toggleLoading(submitButton);
+  }
 };
 
 const setupSecondStep = async job_id => {
@@ -74,9 +85,11 @@ const init = () => {
   // alias error messages 
   const aliasInput = document.querySelector('input[name="job-alias"]');
   const aliasError = document.querySelector('.alias-error');
+  const aliasTaken = document.querySelector('.alias-taken');
   aliasInput.addEventListener('click', e => {
     aliasInput.classList.remove('wrong');
     aliasError.classList.remove('d-block');
+    aliasTaken.classList.remove('d-block');
   });
 
   // get job types and show first form
